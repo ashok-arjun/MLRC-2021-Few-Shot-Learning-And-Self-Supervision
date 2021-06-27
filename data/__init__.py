6dc551967382e046468e2e6f4ac5a3fa019c83b1
Arjun Ashok <arjun2000ashok@gmail.com>
2021-06-27 17:05:18 +0000

    minor

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,6 @@
-from . import datamgr
 from . import datamgr_2loss
-from . import datamgr_unlabel
 
-from . import dataset
 from . import dataset_2loss
-from . import dataset_unlabel
 
 from . import additional_transforms
 from . import feature_loader
